0eaba90a76e24ade9d12707f6567009390136814
Gabriel Nicol <moraesnicol@gmail.com>
2021-06-12 20:42:47 -0300

    pagecontrol ok

---
+++ Gym Purple/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,84 @@
+//
+//  OnboardingViewController.swift
+//  Gym Purple
+//
+//  Created by Gabriel on 12/06/21.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var slides: [OnboardingSlide] = []
+    var currentPage = 0
+    
+
+           
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slides = [
+            OnboardingSlide(title: "Exclusive workout programs", description: "Choose a workout program that best fits to you.", image: #imageLiteral(resourceName: "slide1")),
+            OnboardingSlide(title: "Create custom workouts", description: "Create customized workouts. You have over 500 exercises to choose.", image: #imageLiteral(resourceName: "slide2")),
+            OnboardingSlide(title: "Control your weight and your measurements", description: "Track your progress in charts and statistics.", image: #imageLiteral(resourceName: "slide3"))
+        ]
+        pageControl.numberOfPages = 3
+        
+    }
+    
+
+    
+    @IBAction func pageControlCkicked(_ sender: UIPageControl) {
+        if currentPage == slides.count - 1 {
+         currentPage -= 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true);            print("Go to the next page")
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        }
+  
+    }
+}
+
+
+
+extension OnboardingViewController:
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout,
+    UIScrollViewDelegate
+    {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return slides.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OnboardingCollectionViewCell.identifier, for: indexPath) as! OnboardingCollectionViewCell
+        
+        cell.setup(slides[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+   //
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+           currentPage =  Int(scrollView.contentOffset.x / width)
+     
+    }
+    
+    
+}
