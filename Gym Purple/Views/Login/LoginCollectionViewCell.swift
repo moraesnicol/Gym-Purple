cabb7b7e0819bc8ea511699e6d6843230dc101c6
Gabriel Nicol <moraesnicol@gmail.com>
2021-06-13 16:42:56 -0300

    login buttons added

---
+++ Gym Purple/Views/Login/LoginCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LoginCollectionViewCell.swift
+//  Gym Purple
+//
+//  Created by Gabriel on 13/06/21.
+//
+
+import UIKit
+
+class LoginCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Gym Purple/Views/Login/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Gym Purple
+//
+//  Created by Gabriel on 13/06/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
